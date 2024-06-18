ac2fc6486312c1dea93bb0a4b834328eea601c09
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-06-18 22:45:39 +0545

    Add files via upload

---
+++ SimpleException.java
@@ -0,0 +1,30 @@
+//understanding try and catch block using simple problem
+public class SimpleException {
+
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 0;
+        int nums[] = new int[5];
+
+        String str = null;
+
+        try {
+            j = 18 / i;
+            System.out.println(str.length());
+            System.out.println(nums[5]);
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        // Exception is (parent) class, make sure it is at bottom
+        catch (Exception e) {
+            System.out.println("Something went wrong!!");
+        }
+        System.out.println(j);
+        System.out.println("Byee");
+    }
+
+}
