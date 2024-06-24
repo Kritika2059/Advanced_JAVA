ed16e5095d69a78b73eae287da21abbcee4146c9
Kritika Pandey <110281597+Kritika2059@users.noreply.github.com>
2024-06-24 23:10:57 +0545

    Add files via upload

---
+++ DistributedLab/RmiServerIntf.java
@@ -0,0 +1,15 @@
+package DistributedLab;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RmiServerIntf extends Remote {
+    int add(int a, int b) throws RemoteException;
+
+    int sub(int a, int b) throws RemoteException;
+
+    int mul(int a, int b) throws RemoteException;
+
+    int div(int a, int b) throws RemoteException;
+
+}
